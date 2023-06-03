3a71d224940240f5d37cecaf2e80e69426b85613
liujiahuan <liujiahuan@zbj.com>
2023-06-03 12:00:38 +0800

    merge

--- pages/tuiguang/tuiguang.vue
+++ pages/tuiguang/tuiguang.vue
@@ -42,17 +42,9 @@
 			}
 		},
 		onShow() {
-			this.int()
 		},
 		methods: {
-			int(){
-				// #ifdef H5
-				var a = document.getElementsByClassName('uni-page-head')
-				if (a.length) {
-					a[0].remove()
-				}
-				// #endif
-			},
+			
 		}
 	}
 </script>
