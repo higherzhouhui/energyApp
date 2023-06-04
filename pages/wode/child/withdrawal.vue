683f18fd5e9778dea3aa5e7910bbdc12ef641354
liujiahuan <liujiahuan@zbj.com>
2023-06-04 18:52:53 +0800

    分红提现

--- pages/wode/child/withdrawal.vue
+++ pages/wode/child/withdrawal.vue
@@ -1,28 +1,131 @@
 <template>
 	<view class="container">
 		<view class="top-bg">
+			<view class="top-box">
+				<view class="txt">
+					可提现金额(元)
+				</view>
+				<view class="count-txt">
+					<view class="fh">
+						￥
+					</view>
+						{{price}}
+				</view>
+			</view>
 			
 		</view>
-		<view class="sure-btn">申请提现</view>
+		<view class="top-info">
+			<view class="top-info-box">
+				<view class="txt">
+					提现金额
+				</view>
+				<view class='input-form'>
+					<view class="input-box">
+						<view class="fh">￥</view>
+						<input type="numeric" v-model="txPrice">
+					</view>
+					<view class="all" @tap="txPrice = price">
+						全部提现
+					</view>
+				</view>
+			</view>
+			<view class="sure-btn">申请提现</view>
+		</view>
+		
 	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				price: 10000,
+				txPrice: null
+			}
+		}
+	}
 </script>
 
 <style scoped lang="scss">
 	.container{
 		.top-bg{
+			position: relative;
 			width: 100%;
-			padding-bottom: 69.86%;
+			padding-bottom: 46.4%;
 			background: linear-gradient(180deg, #2EBCFF 0%, #2E96FF 100%);
 			.top-box{
 				position: absolute;
+				padding: 24px 16px;
+				width: 100%;
+				height: 100%;
+				font-size: 14px;
+				font-family: PingFang SC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #fff;
+				.count-txt{
+					margin-top: 5px;
+					font-size: 32px;
+					font-family: DIN-Bold, DIN;
+					font-weight: bold;
+					.fh{
+						display: inline-block;
+						font-size: 20px;
+						font-family: PingFang SC-Regular, PingFang SC;
+						font-weight: 400;
+					}
+				}
+			}
+			
+		}
+		.top-info{
+			width: 93.33%;
+			padding-bottom: 30.07%;
+			position: relative;
+			margin: auto;
+			.top-info-box{
+				position: absolute;
+				padding: 16px;
+				box-sizing: border-box;
+				left: 0;
+				top: -50%;
+				width: 100%;
+				height: 100%;
+				background-color: #fff;
+				border-radius: 8px;
+				font-size: 15px;
+				font-family: PingFang SC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #17191A;
+				.input-form{
+					margin-top: 24px;
+					display: flex;
+					justify-content: space-between;
+					align-items: baseline;
+					.fh{
+						display: inline-block;
+						font-size: 32px;
+						margin-right: 5px;
+					}
+					.input-box{
+						display: flex;
+						align-items: center;
+						input{
+							font-size: 24px;
+							color: #2E96FF;
+						}
+					}
+					.all{
+						font-size: 15px;
+						color: #2E96FF;
+					}
+				}
 			}
 		}
 		.sure-btn{
+			position: absolute;
+			bottom: -24px;
 			padding: 11px 0;
-			width: 86.7%;
+			width: 100%;
 			margin: 24px auto;
 			background-color: rgba(46, 150, 255, 1);
 			color: #fff;
