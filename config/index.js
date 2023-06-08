ece65b767fd8ec7005c3ea2a59a9a4398557bfbf
higherzhouhui <782492184@qq.com>
2023-06-08 17:33:48 +0800

    fix proxy

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 //在config/index.js
 // 请求的基本路径
 const baseURL = process.env.NODE_ENV === "development" ?
-"https://www.zhengtaixinnengyuan.com/energy"://测试环境(请求接口的代理路径)
-"https://www.zhengtaixinnengyuan.com/energy";//上线环境
+"http://www.zhengtaixinnengyuan.com/energy"://测试环境(请求接口的代理路径)
+"http://www.zhengtaixinnengyuan.com/energy";//上线环境
  
 export {baseURL}
