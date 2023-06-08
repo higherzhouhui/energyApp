e117cb9a1058ea77029a0f27a03826a3addac60c
higherzhouhui <782492184@qq.com>
2023-06-08 13:44:15 +0800

    删除多余的文件

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 //在config/index.js
 // 请求的基本路径
 const baseURL = process.env.NODE_ENV === "development" ?
-"https://338v828404.goho.co/energy"://测试环境(请求接口的代理路径)
-"https://338v828404.goho.co/energy";//上线环境
+"http://20.89.128.195:8080/energy"://测试环境(请求接口的代理路径)
+"http://20.89.128.195:8080/energy";//上线环境
  
 export {baseURL}
