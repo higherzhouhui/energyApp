96aff5a250d86e38cb5f484eba9fdad39cb7c5d8
liujiahuan <liujiahuan@zbj.com>
2023-06-09 20:58:15 +0800

    视频

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
 //在config/index.js
 // 请求的基本路径
+
 const baseURL = process.env.NODE_ENV === "development" ?
-"http://www.zhengtaixinnengyuan.com"://测试环境(请求接口的代理路径)
+(window ? '/api' : "http://www.zhengtaixinnengyuan.com")://测试环境(请求接口的代理路径)
 "http://www.zhengtaixinnengyuan.com";//上线环境
  
 export {baseURL}
