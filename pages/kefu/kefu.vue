27367b32917e79e90172591287ed0d9015b8e31f
liujiahuan <liujiahuan@zbj.com>
2023-06-17 22:37:53 +0800

    1

--- pages/kefu/kefu.vue
+++ pages/kefu/kefu.vue
@@ -162,6 +162,7 @@ export default {
                     this.list.push({
                         form: this.userInfo.id,
                         content,
+                        type: t
                         // showTime: (Date.now() - this.times) > this.kTime
                     })
                     this.message = ''
